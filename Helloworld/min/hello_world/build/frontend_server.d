4292eaf860bdb1a6d77145205d35507e73d637e1
minmubi <57170527+minmubi@users.noreply.github.com>
2019-12-14 15:37:49 +0700

    Test

---
+++ Helloworld/min/hello_world/build/frontend_server.d
@@ -0,0 +1,1 @@
+frontend_server.d: D:\Group\yaa hooo\flutter\bin\cache\artifacts\engine\windows-x64\frontend_server.dart.snapshot
---
+++ Helloworld/min/hello_world/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=D:\Group\yaa hooo\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\Group Explo\githubtest\Helloworld\min\hello_world"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=D:\Group\yaa hooo\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
